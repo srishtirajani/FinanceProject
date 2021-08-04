2a4d621547d8091c49e306bc2a5c7d87be37a9e5
Srishti Rajani <43403530+srishtirajani@users.noreply.github.com>
2021-08-04 22:45:20 +0530

    forgot and change password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
+
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 import { LoginComponent } from './components/login/login.component';
+=======
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { ChangepasswordComponent } from './changepassword/changepassword.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     LoginComponent
+=======
+    ForgotpasswordComponent,
+    ChangepasswordComponent,
+    
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
---
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-forgotpassword',
+  templateUrl: './forgotpassword.component.html',
+  styleUrls: ['./forgotpassword.component.css']
+})
+export class ForgotpasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  generateOTP=new FormGroup({
+    phoneno:new FormControl("",[Validators.required,Validators.pattern("^[0-9]+$"), Validators.maxLength(10), Validators.minLength(10)]),
+  })
+  checkOTP=new FormGroup({
+    otp:new FormControl("",[Validators.required,Validators.pattern("^[0-9]+$")]),
+  })
+  Generator(){
+    //alert('hi');
+    console.log(this.generateOTP.value);
+  }
+  Check(){
+    //alert('successful');
+    console.log(this.checkOTP.value);
+  }
+  
+}
+export class NameEditorComponent {
+  phoneno = new FormControl('');
+}
