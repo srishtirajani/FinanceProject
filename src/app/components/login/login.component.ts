0382043ec7133d1ea6e01ebc2820734e52f6f25c
Srishti Rajani <43403530+srishtirajani@users.noreply.github.com>
2021-08-15 20:41:26 +0530

    span added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthGuard } from 'src/app/auth/auth.guard';
@@ -10,9 +10,9 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+ 
   constructor(private loginService:LoginService, private router:Router) { }
-
+  flag = true;
   ngOnInit(): void {
     this.loginService.GetAllLogins().subscribe(data=>{
       this.loginService.logins=data;
@@ -33,16 +33,19 @@ export class LoginComponent implements OnInit {
 
     if(result=="consumer")
     {
+      this.flag=true;
       localStorage.setItem("userName",form.value.userName);
       localStorage.setItem("userName_guard", result);
       this.router.navigate(['dashboard']);
     }
     else if(result=="admin")
     {
+      this.flag=true;
       localStorage.setItem("userName",result);
       this.router.navigate(['admin']);
     }
     else{
+      this.flag=false;
       console.log("Invalid Credentials!");
     }
     form.resetForm();
